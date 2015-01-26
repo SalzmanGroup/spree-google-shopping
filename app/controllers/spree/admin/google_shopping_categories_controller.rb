59d75f9f35ee2d40529286a9a4998ccea0a8ef5f
Kyle Macey <kyle@wholelattelove.com>
2015-01-26 17:40:05 -0500

    allow google shopping category to be mapped to taxon admin interface for bulk updating category maps to taxon

---
+++ app/controllers/spree/admin/google_shopping_categories_controller.rb
@@ -0,0 +1,20 @@
+module Spree
+  module Admin
+    class GoogleShoppingCategoriesController < Spree::Admin::BaseController
+      def index
+        @taxonomies = Taxonomy.includes(:taxons).all
+      end
+      
+      def update_many
+        @taxons = Taxon.update(params[:taxons].keys, params[:taxons].values).reject { |t| t.errors.empty? }
+        if @taxons.empty?
+          flash[:notice] = "Taxons updated"
+          redirect_to admin_google_shopping_categories_path
+        else
+          flash[:error] = "Taxons not updated"
+          render :index
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   namespace :admin do
     resources :google_shopping_integrations
+    resources :google_shopping_categories, only: :index do
+      collection do
+        put :update_many
+      end
+    end
   end
 end
