184bcc845f5221ce0b64f28c1ecc7e25b3870ff3
Kyle Macey <kyle@wholelattelove.com>
2015-02-04 17:31:33 -0500

    add attributes to variants and decorate to accomodate data strcuture of the api

--- db/migrate/20150122195647_add_required_shopping_attributes_to_spree_products.rb
+++
@@ -1,6 +0,0 @@
-class AddRequiredShoppingAttributesToSpreeProducts < ActiveRecord::Migration
-  def change
-    add_column :spree_products, :offerId, :string
-    add_index :spree_products, :offerId
-  end
-end
