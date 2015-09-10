40674f94397ced17e8cb1a1b35c5eb61e4a297dd
Beal <jsbeal@gmail.com>
2015-09-10 15:09:56 -0400

    Update method for shopping integrations, alias to original

--- app/controllers/spree/admin/google_shopping_integrations_controller.rb
+++ app/controllers/spree/admin/google_shopping_integrations_controller.rb
@@ -1,6 +1,13 @@
 module Spree
   module Admin
     class GoogleShoppingIntegrationsController < ResourceController
+
+      alias update original_update
+      def update
+        Rails.logger.info "Here are the params #{params.inspect}"
+        original_update
+      end
+
       def index
         @google_shopping_integrations = GoogleShoppingIntegration.all
       end
