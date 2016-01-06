a0c1c5d684eeb5ae99b6d880a25e00387938ca2f
Beal <jsbeal@gmail.com>
2016-01-06 15:28:34 -0500

    Bumping gem version, adding bundle method and integrating it with previously existing API feed

--- app/models/spree/google_shopping_item.rb
+++ app/models/spree/google_shopping_item.rb
@@ -34,25 +34,25 @@ module Spree
     def self.wrap(collection)
       collection.map(&method(:new))
     end
-    
+
     def self.google_shopping_attributes
       GOOGLE_SHOPPING_ATTRIBUTES
     end
-    
+
     def to_request
       GOOGLE_SHOPPING_ATTRIBUTES.map do |attr|
         [attr[/gs_(.+)/, 1].camelize(:lower), self.send(attr)]
       end.to_h.reject { |k,v| v.blank? }
     end
-    
+
     def gs_offer_id
-      sku.present? ? sku : id 
+      sku.present? ? sku : id
     end
-  
+
     def gs_additional_image_links
       images.drop(1).map { |i| i.attachment.url }
     end
-    
+
     def gs_availability
       if in_stock?
         'in stock'
@@ -62,68 +62,72 @@ module Spree
         'out of stock'
       end
     end
-    
+
     def gs_availability_date
       (product.available_on || 100.years.from_now).iso8601
     end
-    
+
     def gs_description
       product.description
     end
-    
+
     def gs_google_product_category
       taxon = product.taxons.detect { |t| t.google_shopping_category.present? }
       taxon.try(:google_shopping_category)
     end
-    
+
     def gs_identifier_exists
       gs_gtin.present? && gs_gtin[/^\d{8,13}$/]
     end
-    
+
     def gs_image_link
       (images.any? && images.first.attachment.url) || (product.images.any? && product.images.first.attachment.url)
     end
-    
+
     def gs_item_group_id
       product_id
     end
-    
+
     def gs_link
       product_path(product)
     end
-    
+
     def gs_mpn
       sku
     end
-    
+
     def gs_price
       {
         value: default_price.amount.to_f,
         currency: default_price.currency
       }
     end
-    
+
     def gs_product_type
       product.taxons.map { |t| t.self_and_ancestors.pluck(:name).join(' > ') }.join(', ')
     end
-    
+
     def gs_shipping_weight
       {
         value: [weight.to_i, 1].max,
         unit: Spree::Config.shipping_weight_unit
       }
     end
-    
+
     def gs_title
       product.name
     end
-    
+
+    def bundle?
+      gs_bundle == true
+    end
+
     private
-    
+
     def color_option
       @_color_option ||= Rails.cache.fetch(['color_option', Spree::OptionType.maximum(:created_at)]) {
         Spree::OptionType.find_by name: 'Color'
       }
-    end    
+    end
   end
 end
--- app/services/spree/insert_products_collection_to_google_shopping.rb
+++ app/services/spree/insert_products_collection_to_google_shopping.rb
@@ -1,6 +1,6 @@
 module Spree
   class InsertProductsCollectionToGoogleShopping
-    
+
     def self.call(products, google_shopping_integration)
       variants = products.includes(:taxons, master: [:images, :stock_items, :default_price], variants: [:images, :stock_items, :default_price]).flat_map do |p|
         p.variants.any? ? p.variants : p.master
@@ -10,17 +10,18 @@ module Spree
       entries = @google_shopping_items.map { |item| bulk_attributes(item) }
       success?(google_shopping_client.batch_operation(entries))
     end
-    
+
     private
-    
+
     def self.google_shopping_client
       @google_shopping_integration.client
     end
-    
+
     def self.bulk_attributes(google_shopping_item)
       {
         merchantId: @google_shopping_integration.merchant_id,
-        batchId: google_shopping_item.id, 
+        batchId: google_shopping_item.id,
+        isBundle: google_shopping_item.gs_bundle,
         method: 'insert',
         product: google_shopping_item.to_request.merge({
           channel: @google_shopping_integration.channel,
@@ -30,11 +31,11 @@ module Spree
         })
       }
     end
-    
+
     def self.route_helpers
       Spree::Core::Engine.routes.url_helpers
     end
-    
+
     def self.success?(message)
       message.response.status == 200
     end
